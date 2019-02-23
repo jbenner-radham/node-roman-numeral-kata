65a4b20bb71d6c94bc2a10e651412964304e93b1
James Benner <james.benner@gmail.com>
2019-02-23 16:43:13 -0500

    Refactored app to convert.

--- test/roman-numeral-converter.spec.js
+++ test/roman-numeral-converter.spec.js
@@ -1,68 +1,68 @@
 const { expect } = require('chai');
-const app = require('../lib');
+const convert = require('../lib');
 
-describe('Roman Numeral App', function () {
+describe('Roman Numeral Converter', function () {
     it('is a function', function () {
-        expect(app).to.be.a('function');
+        expect(convert).to.be.a('function');
     });
 
     it('converts I to 1', function () {
-        expect(app('I')).to.equal(1);
+        expect(convert('I')).to.equal(1);
     });
 
     it('converts V to 5', function () {
-        expect(app('V')).to.equal(5);
+        expect(convert('V')).to.equal(5);
     });
 
     it('converts X to 10', function () {
-        expect(app('X')).to.equal(10);
+        expect(convert('X')).to.equal(10);
     });
 
     it('converts L to 50', function () {
-        expect(app('L')).to.equal(50);
+        expect(convert('L')).to.equal(50);
     });
 
     it('converts C to 100', function () {
-        expect(app('C')).to.equal(100);
+        expect(convert('C')).to.equal(100);
     });
 
     it('converts D to 500', function () {
-        expect(app('D')).to.equal(500);
+        expect(convert('D')).to.equal(500);
     });
 
     it('converts M to 1000', function () {
-        expect(app('M')).to.equal(1000);
+        expect(convert('M')).to.equal(1000);
     });
 
     it('converts IV to 4', function () {
-        expect(app('IV')).to.equal(4);
+        expect(convert('IV')).to.equal(4);
     });
 
     it('converts VI to 6', function () {
-        expect(app('VI')).to.equal(6);
+        expect(convert('VI')).to.equal(6);
     });
 
     it('converts VII to 7', function () {
-        expect(app('VII')).to.equal(7);
+        expect(convert('VII')).to.equal(7);
     });
 
     it('converts VIII to 8', function () {
-        expect(app('VIII')).to.equal(8);
+        expect(convert('VIII')).to.equal(8);
     });
 
     it('converts VIIII to NaN', function () {
-        expect(app('VIIII')).to.eql(NaN);
+        expect(convert('VIIII')).to.eql(NaN);
     });
 
     it('converts IX to 9', function () {
-        expect(app('IX')).to.equal(9);
+        expect(convert('IX')).to.equal(9);
     });
 
     it('converts MMXIX to 2019', function () {
-        expect(app('MMXIX')).to.equal(2019);
+        expect(convert('MMXIX')).to.equal(2019);
     });
 
     it('converts VV to to NaN', function () {
-        expect(app('VV')).to.eql(NaN);
+        expect(convert('VV')).to.eql(NaN);
     });
 });
